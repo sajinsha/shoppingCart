d2e80c2e1b94e498668cf5f5da0b102dd172c3a0
Sajinsha <84446813+sajinsha@users.noreply.github.com>
2021-11-17 16:35:00 +0530

    save arezzo files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react'
+import Data from "./data";
+import Navbar from "./Navbar"
+import "./App.css"
+import Main from "./Main";
+import Basket from "./Basket"
 
 function App() {
+const {products} = Data
+const[cartItems,setCartItems] = useState([])
+
+const onAdd = (product) => {
+ const exist = cartItems.find((x) => x.id === product.id) ;
+ if(exist){
+   setCartItems(cartItems.map((x) => x.id === product.id ? {...exist, qty : exist.qty + 1 } : x ))
+} else {
+  setCartItems([...cartItems, {...product,qty:1}])
+}
+};
+
+
+
+
+
+const onRemove = (product) => {
+  const exist = cartItems.find((x) => x.id === product.id);
+  if (exist.qty === 1) {
+    setCartItems(cartItems.filter((x) => x.id !== product.id));
+  } else {
+    setCartItems(
+      cartItems.map((x) =>
+        x.id === product.id ? { ...exist, qty: exist.qty - 1 } : x
+        
+      )
+    );
+  }
+};
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Navbar cartItems={cartItems}/>
+     <Main products={products}  onAdd={onAdd}/>
+     <hr/>
+     <Basket cartItems={cartItems} onAdd={onAdd} onRemove={onRemove}/>
     </div>
   );
 }
